0210ee0a839e92d3c05c1b2caa559a7f75508328
Eric Chadwick <eric.chadwick@cerner.com>
2017-01-02 22:02:41 -0600

    add validations on recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,4 @@
 class Recipe < ActiveRecord::Base
+  validates :name, uniqueness: true, presence: {message: 'Recipe name is required'}
+  validates :directions, presence: {message: 'Recipe directions are required'}
 end
