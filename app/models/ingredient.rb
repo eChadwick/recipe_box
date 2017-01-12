dd93cd9baf0cdccbc1606d1859785c05c5aeddc3
Eric Chadwick <eric.chadwick@cerner.com>
2017-01-12 10:10:30 -0600

    add custom validation message to ingredients model

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,4 @@
 class Ingredient < ActiveRecord::Base
-  validates :name, uniqueness: true, presence: { message: 'Ingredient name is required' }
+  validates :name, presence: { message: 'Ingredient name is required' }
   validates :name, uniqueness: { message: 'Ingredient with that name already exists' }
 end
