fec7bbb79c5dc72eec8b540da42bd53c7558321a
Eric Chadwick <eChadwick@users.noreply.github.com>
2018-01-09 22:09:28 -0600

    interactive add/delete recipe ingredients

--- app/assets/javascripts/add_ingredient.js
+++ app/assets/javascripts/add_ingredient.js
@@ -9,9 +9,18 @@ function addIngredient(){
   measurement.placeholder = "Measurement";
   measurement.name = "[recipe_ingredients][" + id + "][measurement]";
 
+  var remove_button = document.createElement("img");
+  remove_button.src = "/assets/remove-button.png";
+  remove_button.className = "remove_button";
+  remove_button.onclick = function() {
+    var target = this.parentElement;
+    target.parentElement.removeChild(target);
+  }
+
   var td = document.createElement("td");
   td.appendChild(ingredient);
   td.appendChild(measurement);
+  td.appendChild(remove_button);
 
   var tr = document.createElement("tr");
   tr.appendChild(td);
