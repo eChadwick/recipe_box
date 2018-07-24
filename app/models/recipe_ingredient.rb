b4662e1cea2fc1e722678c741d652bf3868e27eb
Eric Chadwick <eChadwick@users.noreply.github.com>
2018-07-24 13:24:24 -0500

    fix ingredient duplication issue

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -2,5 +2,17 @@ class RecipeIngredient < ActiveRecord::Base
   belongs_to :recipe, validate: true
   belongs_to :ingredient, validate: true
 
-  accepts_nested_attributes_for :ingredient
+  accepts_nested_attributes_for :ingredient, reject_if: :ingredient_exists?
+
+  protected
+
+    def ingredient_exists?(ingredient_attrs)
+      ingredient_attrs[:name].capitalize!
+      if ingredient = Ingredient.find_by(name: ingredient_attrs[:name])
+        self.ingredient = ingredient
+        return true
+      else
+        return false
+      end
+    end
 end
